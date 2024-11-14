f5cb2bd0288960242a94e5e9b553725acce1dd86
Arushitester <af.arushi@gmail.com>
2024-11-14 16:05:37 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver =new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://www.amazon.in");
+        String act  =driver.getTitle();
+        if(act.equals("amazon.in"))
+        {
+            System.out.println("Right title");
+
+        }
+        else {
+
+            System.out.println("Wrong title");
+        }
+    }
+}
